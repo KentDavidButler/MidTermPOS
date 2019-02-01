c1d794fe488f54e3465784a512d5d19ac2fa4c4c
stevojed <jedi1014@comcast.net>
2019-02-01 11:47:12 -0500

    Databases

---
+++ MidTermPOS/DepressantDB.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidTermPOS
+{
+    class DepressantDB
+    {
+        List<DrugType> DrugName = new List<DrugType>();
+
+        public DepressantDB()
+        {
+            DrugName.Add(new DrugType("SleepEZ"));
+            DrugName.Add(new DrugType("Xanax"));
+            DrugName.Add(new DrugType("Klonopin"));
+            DrugName.Add(new DrugType("Valium"));
+        }
+    }
+}
---
+++ MidTermPOS/StimulantsDB.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidTermPOS
+{
+    class StimulantsDB
+    {
+        public List<DrugType> DrugName = new List<DrugType>();
+
+        public StimulantsDB()
+        {
+            DrugName.Add(new DrugType("Adderall"));
+            DrugName.Add(new DrugType("Ritalin"));
+            DrugName.Add(new DrugType("Concerta"));
+            DrugName.Add(new DrugType("Focalin"));
+        }
+    }
+}
